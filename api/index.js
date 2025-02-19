5d426c18eda28d6f777962804baac269f0f6e87e
Edric Huang <Edric Huang>
2025-02-19 15:52:17 +0100

    Add model patient.js Update utilisateur endpoints in index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,75 @@
 const express = require('express');
-const Book = require('./models/book');
+const Utilisateur = require('./models/utilisateur');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+
+
+
+// Endpoints utilisateur
+
+app.get('/utilisateur', async (req, res) => {
+    try {
+        const utilisateur = await Utilisateur.getAllUser();
+        res.status(200).json(utilisateur);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/utilisateur/:id_utilisateur', async (req, res) => {
+    try {
+        const utilisateur = await Utilisateur.getUserById(req.params.id_utilisateur);
+        utilisateur ? res.status(200).json(utilisateur) : res.status(404).json({ message: "utilisateur non trouvé" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.post('/utilisateur/connexion', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const utilisateur = await Utilisateur.getUserByEmail(email);
+
+        if (utilisateur && use.password === password) {
+            res.status(200).json({ message: "Connexion réussie", utilisateur });
+        } else {
+            res.status(401).json({ message: "Email ou mot de passe incorrect" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.post('/utilisateur', async (req, res) => {
+    try {
+        const newuser = await Utilisateur.createUser(req.body);
+        res.status(201).json({ newuser });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.put('/utilisateur/:id_utilisateur', async (req, res) => {
+    try {
+        const updateuser = await Utilisateur.updateUser(req.params.id_utilisateur, req.body);
+        res.status(200).json({ updateuser });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.delete('/utilisateur/:id_utilisateur', async (req, res) => {
+    try {
+        await Utilisateur.deleteUser(req.params.id_utilisateur);
+        res.status(204).send();
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur le port ${PORT}`);
+});
