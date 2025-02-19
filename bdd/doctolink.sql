8646238a46c859eb512d7ff27b4e62cf27eb5937
Edric Huang <Edric Huang>
2025-02-19 16:42:47 +0100

    Update doctolink.js

--- bdd/doctolink.sql
+++ bdd/doctolink.sql
@@ -11,13 +11,13 @@ CREATE TABLE patient(
    sexe VARCHAR(50) NOT NULL,
    date_de_naissance DATE NOT NULL,
    id_utilisateur INT NOT NULL UNIQUE,
-   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur)
+   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE
 );
 
 CREATE TABLE administration(
    id_administration SERIAL PRIMARY KEY,
    id_utilisateur INT NOT NULL UNIQUE,
-   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur)
+   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE
 );
 
 CREATE TABLE specialite(
@@ -60,13 +60,13 @@ CREATE TABLE soignant(
    id_etablissement INT NOT NULL,
    id_utilisateur INT NOT NULL UNIQUE,
    FOREIGN KEY(id_etablissement) REFERENCES etablissement(id_etablissement),
-   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur)
+   FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE
 );
 
 CREATE TABLE dossier_medical(
    id_dossier_medical SERIAL PRIMARY KEY,
    id_patient INT NOT NULL,
-   FOREIGN KEY(id_patient) REFERENCES patient(id_patient)
+   FOREIGN KEY(id_patient) REFERENCES patient(id_patient) ON DELETE CASCADE
 );
 
 CREATE TABLE rendez_vous(
@@ -75,8 +75,8 @@ CREATE TABLE rendez_vous(
    statut VARCHAR(50) NOT NULL,
    id_soignant INT NOT NULL,
    id_patient INT NOT NULL,
-   FOREIGN KEY(id_soignant) REFERENCES soignant(id_soignant),
-   FOREIGN KEY(id_patient) REFERENCES patient(id_patient)
+   FOREIGN KEY(id_soignant) REFERENCES soignant(id_soignant) ON DELETE CASCADE,
+   FOREIGN KEY(id_patient) REFERENCES patient(id_patient) ON DELETE CASCADE
 );
 
 CREATE TABLE facturation(
@@ -86,7 +86,7 @@ CREATE TABLE facturation(
    id_rendez_vous INT NOT NULL UNIQUE,
    id_assurance_sante INT,
    FOREIGN KEY(id_mode_de_paiment) REFERENCES mode_de_paiment(id_mode_de_paiment),
-   FOREIGN KEY(id_rendez_vous) REFERENCES rendez_vous(id_rendez_vous),
+   FOREIGN KEY(id_rendez_vous) REFERENCES rendez_vous(id_rendez_vous) ON DELETE CASCADE,
    FOREIGN KEY(id_assurance_sante) REFERENCES assurance_sante(id_assurance_sante)
 );
 
@@ -98,7 +98,7 @@ CREATE TABLE historique_medicale(
    id_soignant INT NOT NULL,
    id_dossier_medical INT NOT NULL,
    FOREIGN KEY(id_soignant) REFERENCES soignant(id_soignant),
-   FOREIGN KEY(id_dossier_medical) REFERENCES dossier_medical(id_dossier_medical)
+   FOREIGN KEY(id_dossier_medical) REFERENCES dossier_medical(id_dossier_medical) ON DELETE CASCADE
 );
 
 CREATE TABLE synthese_medicale(
@@ -109,7 +109,7 @@ CREATE TABLE synthese_medicale(
    id_dossier_medical INT NOT NULL,
    id_type_synthese_medicale INT NOT NULL,
    FOREIGN KEY(id_soignant) REFERENCES soignant(id_soignant),
-   FOREIGN KEY(id_dossier_medical) REFERENCES dossier_medical(id_dossier_medical),
+   FOREIGN KEY(id_dossier_medical) REFERENCES dossier_medical(id_dossier_medical) ON DELETE CASCADE,
    FOREIGN KEY(id_type_synthese_medicale) REFERENCES type_synthese_medicale(id_type_synthese_medicale)
 );
 
@@ -117,38 +117,38 @@ CREATE TABLE administration_etablissement(
    id_administration INT,
    id_etablissement INT,
    PRIMARY KEY(id_administration, id_etablissement),
-   FOREIGN KEY(id_administration) REFERENCES administration(id_administration),
-   FOREIGN KEY(id_etablissement) REFERENCES etablissement(id_etablissement)
+   FOREIGN KEY(id_administration) REFERENCES administration(id_administration) ON DELETE CASCADE,
+   FOREIGN KEY(id_etablissement) REFERENCES etablissement(id_etablissement) ON DELETE CASCADE
 );
 
 CREATE TABLE administration_soignant(
    id_administration INT,
    id_soignant INT,
    PRIMARY KEY(id_administration, id_soignant),
-   FOREIGN KEY(id_administration) REFERENCES administration(id_administration),
-   FOREIGN KEY(id_soignant) REFERENCES soignant(id_soignant)
+   FOREIGN KEY(id_administration) REFERENCES administration(id_administration) ON DELETE CASCADE,
+   FOREIGN KEY(id_soignant) REFERENCES soignant(id_soignant) ON DELETE CASCADE
 );
 
 CREATE TABLE administration_patient(
    id_patient INT,
    id_administration INT,
    PRIMARY KEY(id_patient, id_administration),
-   FOREIGN KEY(id_patient) REFERENCES patient(id_patient),
-   FOREIGN KEY(id_administration) REFERENCES administration(id_administration)
+   FOREIGN KEY(id_patient) REFERENCES patient(id_patient) ON DELETE CASCADE,
+   FOREIGN KEY(id_administration) REFERENCES administration(id_administration) ON DELETE CASCADE
 );
 
 CREATE TABLE administration_rendez_vous(
    id_administration INT,
    id_rendez_vous INT,
    PRIMARY KEY(id_administration, id_rendez_vous),
-   FOREIGN KEY(id_administration) REFERENCES administration(id_administration),
-   FOREIGN KEY(id_rendez_vous) REFERENCES rendez_vous(id_rendez_vous)
+   FOREIGN KEY(id_administration) REFERENCES administration(id_administration) ON DELETE CASCADE,
+   FOREIGN KEY(id_rendez_vous) REFERENCES rendez_vous(id_rendez_vous) ON DELETE CASCADE
 );
 
 CREATE TABLE soignant_specialite(
    id_soignant INT,
    id_specialite INT,
    PRIMARY KEY(id_soignant, id_specialite),
-   FOREIGN KEY(id_soignant) REFERENCES soignant(id_soignant),
-   FOREIGN KEY(id_specialite) REFERENCES specialite(id_specialite)
+   FOREIGN KEY(id_soignant) REFERENCES soignant(id_soignant) ON DELETE CASCADE,
+   FOREIGN KEY(id_specialite) REFERENCES specialite(id_specialite) ON DELETE CASCADE
 );
